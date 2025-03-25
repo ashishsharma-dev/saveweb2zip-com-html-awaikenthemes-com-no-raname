73fa47487e7a4e3438a89e99849eb77f1a17773f
Ashish Sharma <sharmaashish049@gmail.com>
2025-03-26 01:49:48 +0530

    Measure Changes done related to contact page, course page, single course page and homepage modal

--- js/function.js
+++ js/function.js
@@ -1,267 +1,342 @@
 (function ($) {
-    "use strict";
-	
-	var $window = $(window); 
-	var $body = $('body'); 
+  "use strict";
 
-	/* Preloader Effect */
-	$window.on('load', function(){
-		$(".preloader").fadeOut(600);
-	});
+  var $window = $(window);
+  var $body = $("body");
 
-	/* Sticky Header */	
-	if($('.active-sticky-header').length){
-		$window.on('resize', function(){
-			setHeaderHeight();
-		});
+  /* Preloader Effect */
+  $window.on("load", function () {
+    $(".preloader").fadeOut(600);
+  });
 
-		function setHeaderHeight(){
-	 		$("header.main-header").css("height", $('header .header-sticky').outerHeight());
-		}	
-	
-		$(window).on("scroll", function() {
-			var fromTop = $(window).scrollTop();
-			setHeaderHeight();
-			var headerHeight = $('header .header-sticky').outerHeight()
-			$("header .header-sticky").toggleClass("hide", (fromTop > headerHeight + 100));
-			$("header .header-sticky").toggleClass("active", (fromTop > 600));
-		});
-	}	
-	
-	/* Slick Menu JS */
-	$('#menu').slicknav({
-		label : '',
-		prependTo : '.responsive-menu'
-	});
+  /* Sticky Header */
+  if ($(".active-sticky-header").length) {
+    $window.on("resize", function () {
+      setHeaderHeight();
+    });
 
-	if($("a[href='#top']").length){
-		$("a[href='#top']").click(function() {
-			$("html, body").animate({ scrollTop: 0 }, "slow");
-			return false;
-		});
-	}
-
-	/* Hero Slider Layout JS */
-	const hero_slider_layout = new Swiper('.hero-slider-layout .swiper', {
-		slidesPerView : 1,
-		speed: 1000,
-		spaceBetween: 0,
-		loop: true,
-		autoplay: {
-			delay: 4000,
-		},
-		pagination: {
-			el: '.hero-pagination',
-			clickable: true,
-		},
-	});
-
-	/* testimonial Slider JS */
-	if ($('.testimonial-slider').length) {
-		const testimonial_slider = new Swiper('.testimonial-slider .swiper', {
-			slidesPerView : 1,
-			speed: 1000,
-			spaceBetween: 30,
-			loop: true,
-			autoplay: {
-				delay: 5000,
-			},
-			pagination: {
-				el: '.swiper-pagination',
-				clickable: true,
-			},
-			navigation: {
-				nextEl: '.testimonial-button-next',
-				prevEl: '.testimonial-button-prev',
-			},
-			breakpoints: {
-				768:{
-				  	slidesPerView: 1,
-				},
-				991:{
-				  	slidesPerView: 1,
-				}
-			}
-		});
-	}
-
-	/* Skill Bar */
-	if ($('.skills-progress-bar').length) {
-		$('.skills-progress-bar').waypoint(function() {
-			$('.skillbar').each(function() {
-				$(this).find('.count-bar').animate({
-				width:$(this).attr('data-percent')
-				},2000);
-			});
-		},{
-			offset: '70%'
-		});
-	}
-
-	/* Youtube Background Video JS */
-	if ($('#herovideo').length) {
-		var myPlayer = $("#herovideo").YTPlayer();
-	}
-
-	/* Init Counter */
-	if ($('.counter').length) {
-		$('.counter').counterUp({ delay: 6, time: 3000 });
-	}
-
-	/* Image Reveal Animation */
-	if ($('.reveal').length) {
-        gsap.registerPlugin(ScrollTrigger);
-        let revealContainers = document.querySelectorAll(".reveal");
-        revealContainers.forEach((container) => {
-            let image = container.querySelector("img");
-            let tl = gsap.timeline({
-                scrollTrigger: {
-                    trigger: container,
-                    toggleActions: "play none none none"
-                }
-            });
-            tl.set(container, {
-                autoAlpha: 1
-            });
-            tl.from(container, 1, {
-                xPercent: -100,
-                ease: Power2.out
-            });
-            tl.from(image, 1, {
-                xPercent: 100,
-                scale: 1,
-                delay: -1,
-                ease: Power2.out
-            });
-        });
+    function setHeaderHeight() {
+      $("header.main-header").css(
+        "height",
+        $("header .header-sticky").outerHeight()
+      );
     }
 
-	/* Parallaxie js */
-	var $parallaxie = $('.parallaxie');
-	if($parallaxie.length && ($window.width() > 991))
-	{
-		if ($window.width() > 768) {
-			$parallaxie.parallaxie({
-				speed: 0.55,
-				offset: 0,
-			});
-		}
-	}
+    $(window).on("scroll", function () {
+      var fromTop = $(window).scrollTop();
+      setHeaderHeight();
+      var headerHeight = $("header .header-sticky").outerHeight();
+      $("header .header-sticky").toggleClass(
+        "hide",
+        fromTop > headerHeight + 100
+      );
+      $("header .header-sticky").toggleClass("active", fromTop > 600);
+    });
+  }
 
-	/* Zoom Gallery screenshot */
-	$('.gallery-items').magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		closeOnContentClick: false,
-		closeBtnInside: false,
-		mainClass: 'mfp-with-zoom',
-		image: {
-			verticalFit: true,
-		},
-		gallery: {
-			enabled: true
-		},
-		zoom: {
-			enabled: true,
-			duration: 300, // don't foget to change the duration also in CSS
-			opener: function(element) {
-			  return element.find('img');
-			}
-		}
-	});
+  /* Slick Menu JS */
+  $("#menu").slicknav({
+    label: "",
+    prependTo: ".responsive-menu",
+  });
 
-	/* Contact form validation */
-	var $contactform = $("#contactForm");
-	$contactform.validator({focus: false}).on("submit", function (event) {
-		if (!event.isDefaultPrevented()) {
-			event.preventDefault();
-			submitForm();
-		}
-	});
+  if ($("a[href='#top']").length) {
+    $("a[href='#top']").click(function () {
+      $("html, body").animate({ scrollTop: 0 }, "slow");
+      return false;
+    });
+  }
 
-	function submitForm(){
-		/* Ajax call to submit form */
-		$.ajax({
-			type: "POST",
-			url: "form-process.php",
-			data: $contactform.serialize(),
-			success : function(text){
-				if (text == "success"){
-					formSuccess();
-				} else {
-					submitMSG(false,text);
-				}
-			}
-		});
-	}
+  /* Hero Slider Layout JS */
+  const hero_slider_layout = new Swiper(".hero-slider-layout .swiper", {
+    slidesPerView: 1,
+    speed: 1000,
+    spaceBetween: 0,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+    },
+    pagination: {
+      el: ".hero-pagination",
+      clickable: true,
+    },
+  });
 
-	function formSuccess(){
-		$contactform[0].reset();
-		submitMSG(true, "Message Sent Successfully!")
-	}
+  /* testimonial Slider JS */
+  if ($(".testimonial-slider").length) {
+    const testimonial_slider = new Swiper(".testimonial-slider .swiper", {
+      slidesPerView: 1,
+      speed: 1000,
+      spaceBetween: 30,
+      loop: true,
+      autoplay: {
+        delay: 5000,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".testimonial-button-next",
+        prevEl: ".testimonial-button-prev",
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 1,
+        },
+        991: {
+          slidesPerView: 1,
+        },
+      },
+    });
+  }
 
-	function submitMSG(valid, msg){
-		if(valid){
-			var msgClasses = "h4 text-success";
-		} else {
-			var msgClasses = "h4 text-danger";
-		}
-		$("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
-	}
-	/* Contact form validation end */
+  /* Skill Bar */
+  if ($(".skills-progress-bar").length) {
+    $(".skills-progress-bar").waypoint(
+      function () {
+        $(".skillbar").each(function () {
+          $(this)
+            .find(".count-bar")
+            .animate(
+              {
+                width: $(this).attr("data-percent"),
+              },
+              2000
+            );
+        });
+      },
+      {
+        offset: "70%",
+      }
+    );
+  }
 
-	/* Appointment form validation */
-	var $appointmentForm = $("#appointmentForm");
-	$appointmentForm.validator({focus: false}).on("submit", function (event) {
-		if (!event.isDefaultPrevented()) {
-			event.preventDefault();
-			submitappointmentForm();
-		}
-	});
+  /* Youtube Background Video JS */
+  if ($("#herovideo").length) {
+    var myPlayer = $("#herovideo").YTPlayer();
+  }
 
-	function submitappointmentForm(){
-		/* Ajax call to submit form */
-		$.ajax({
-			type: "POST",
-			url: "form-appointment.php",
-			data: $appointmentForm.serialize(),
-			success : function(text){
-				if (text == "success"){
-					appointmentformSuccess();
-				} else {
-					appointmentsubmitMSG(false,text);
-				}
-			}
-		});
-	}
+  /* Init Counter */
+  if ($(".counter").length) {
+    $(".counter").counterUp({ delay: 6, time: 3000 });
+  }
 
-	function appointmentformSuccess(){
-		$appointmentForm[0].reset();
-		appointmentsubmitMSG(true, "Message Sent Successfully!")
-	}
+  /* Image Reveal Animation */
+  if ($(".reveal").length) {
+    gsap.registerPlugin(ScrollTrigger);
+    let revealContainers = document.querySelectorAll(".reveal");
+    revealContainers.forEach((container) => {
+      let image = container.querySelector("img");
+      let tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: container,
+          toggleActions: "play none none none",
+        },
+      });
+      tl.set(container, {
+        autoAlpha: 1,
+      });
+      tl.from(container, 1, {
+        xPercent: -100,
+        ease: Power2.out,
+      });
+      tl.from(image, 1, {
+        xPercent: 100,
+        scale: 1,
+        delay: -1,
+        ease: Power2.out,
+      });
+    });
+  }
 
-	function appointmentsubmitMSG(valid, msg){
-		if(valid){
-			var msgClasses = "h3 text-success";
-		} else {
-			var msgClasses = "h3 text-danger";
-		}
-		$("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
-	}
-	/* Appointment form validation end */
+  /* Parallaxie js */
+  var $parallaxie = $(".parallaxie");
+  if ($parallaxie.length && $window.width() > 991) {
+    if ($window.width() > 768) {
+      $parallaxie.parallaxie({
+        speed: 0.55,
+        offset: 0,
+      });
+    }
+  }
 
-	/* Animated Wow Js */	
-	new WOW().init();
+  /* Zoom Gallery screenshot */
+  $(".gallery-items").magnificPopup({
+    delegate: "a",
+    type: "image",
+    closeOnContentClick: false,
+    closeBtnInside: false,
+    mainClass: "mfp-with-zoom",
+    image: {
+      verticalFit: true,
+    },
+    gallery: {
+      enabled: true,
+    },
+    zoom: {
+      enabled: true,
+      duration: 300, // don't foget to change the duration also in CSS
+      opener: function (element) {
+        return element.find("img");
+      },
+    },
+  });
 
-	/* Popup Video */
-	if ($('.popup-video').length) {
-		$('.popup-video').magnificPopup({
-			type: 'iframe',
-			mainClass: 'mfp-fade',
-			removalDelay: 160,
-			preloader: false,
-			fixedContentPos: true
-		});
-	}
-	
+  /* Contact form validation */
+  var $contactform = $("#contactForm");
+  $contactform.validator({ focus: false }).on("submit", function (event) {
+    if (!event.isDefaultPrevented()) {
+      event.preventDefault();
+      submitForm();
+    }
+  });
+
+  function submitForm() {
+    /* Ajax call to submit form */
+    $.ajax({
+      type: "POST",
+      url: "form-process.php",
+      data: $contactform.serialize(),
+      success: function (text) {
+        if (text == "success") {
+          formSuccess();
+        } else {
+          submitMSG(false, text);
+        }
+      },
+    });
+  }
+
+  function formSuccess() {
+    $contactform[0].reset();
+    submitMSG(true, "Message Sent Successfully!");
+  }
+
+  function submitMSG(valid, msg) {
+    if (valid) {
+      var msgClasses = "h4 text-success";
+    } else {
+      var msgClasses = "h4 text-danger";
+    }
+    $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
+  }
+  /* Contact form validation end */
+
+  /* Appointment form validation */
+  var $appointmentForm = $("#appointmentForm");
+  $appointmentForm.validator({ focus: false }).on("submit", function (event) {
+    if (!event.isDefaultPrevented()) {
+      event.preventDefault();
+      submitappointmentForm();
+    }
+  });
+
+  function submitappointmentForm() {
+    /* Ajax call to submit form */
+    $.ajax({
+      type: "POST",
+      url: "form-appointment.php",
+      data: $appointmentForm.serialize(),
+      success: function (text) {
+        if (text == "success") {
+          appointmentformSuccess();
+        } else {
+          appointmentsubmitMSG(false, text);
+        }
+      },
+    });
+  }
+
+  function appointmentformSuccess() {
+    $appointmentForm[0].reset();
+    appointmentsubmitMSG(true, "Message Sent Successfully!");
+  }
+
+  function appointmentsubmitMSG(valid, msg) {
+    if (valid) {
+      var msgClasses = "h3 text-success";
+    } else {
+      var msgClasses = "h3 text-danger";
+    }
+    $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
+  }
+  /* Appointment form validation end */
+
+  /* Animated Wow Js */
+  new WOW().init();
+
+  /* Popup Video */
+  if ($(".popup-video").length) {
+    $(".popup-video").magnificPopup({
+      type: "iframe",
+      mainClass: "mfp-fade",
+      removalDelay: 160,
+      preloader: false,
+      fixedContentPos: true,
+    });
+  }
 })(jQuery);
+
+// Handling Contact form
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbzEKD8FDz9mFS8aSOgTUzRyumeOcwtmoQCByJJwYidZQKEM-gWeXyOno6bXThGc0xAA/exec";
+const form = document.forms["submit-to-google-sheet"];
+const message = document.getElementById("msgSubmit");
+const currLocation = document.getElementById("CurrLocation");
+const closeBtn = document.querySelector(".close-btn");
+const submitBtn = document.querySelector(
+  ".contact-form-btn button[type='submit']"
+);
+const loadingAnimation = document.querySelector(".loading-animation");
+
+if (form) {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    currLocation.value = window.location.href;
+
+    submitBtn.disabled = true;
+    loadingAnimation.classList.remove("hidden");
+
+    fetch(scriptURL, { method: "POST", body: new FormData(form) })
+      .then((response) => {
+        message.classList.remove("hidden");
+        submitBtn.disabled = false;
+        loadingAnimation.classList.add("hidden");
+        if (closeBtn) {
+          closeBtn.click();
+        }
+        form.reset();
+        setTimeout(() => {
+          message.classList.add("hidden");
+        }, 3000);
+        console.log("Success!", response);
+      })
+      .catch((error) => console.error("Error!", error.message));
+  });
+}
+
+// Handling Enrollment form modal
+
+const modal = document.getElementById("enroll-modal");
+const openModalBtn = document.getElementById("openModalBtn");
+const closeModalBtn = modal.querySelector(".close-modal-btn");
+
+// Open modal
+openModalBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  modal.classList.add("show");
+});
+
+// Close modal
+closeModalBtn.addEventListener("click", () => {
+  modal.classList.remove("show");
+});
+
+// Close when clicking outside modal content
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.classList.remove("show");
+  }
+});
